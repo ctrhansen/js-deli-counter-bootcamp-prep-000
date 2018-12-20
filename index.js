523d3ede313f1f1383d03d8c6c1ea79fbc0260e5
Thomas Hansen <ctrhansen@yahoo.com>
2018-12-20 16:46:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function takeANumber(currLine, name) {
   currLine.push(name)
-  return (`Welcome, ${name}. You are number ${currLine.length} in line.`)
+
 }
+  return (`Welcome, ${name}. You are number ${currLine.length} in line.`)
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return 'There is nobody waiting to be served!'
